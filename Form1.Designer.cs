e6c09521c5f1349f79a0e15d6661e357d42ce1da
Mengke <35460065+Jone-Mark@users.noreply.github.com>
2018-10-23 09:16:11 +0800

    STM32_IO_SET-1
    STM32_IO_SET-1

---
+++ Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "A",
+            "B",
+            "C",
+            "D",
+            "E",
+            "F",
+            "G"});
+            this.comboBox1.Location = new System.Drawing.Point(39, 72);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(62, 20);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.Text = "A";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15"});
+            this.comboBox2.Location = new System.Drawing.Point(144, 72);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(69, 20);
+            this.comboBox2.TabIndex = 7;
+            this.comboBox2.Text = "0";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "模拟输入",
+            "浮空输入",
+            "上拉输入",
+            "下拉输入",
+            "通用开漏输出",
+            "通用推挽输出",
+            "复用开漏输出",
+            "复用推挽输出"});
+            this.comboBox3.Location = new System.Drawing.Point(256, 72);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 20);
+            this.comboBox3.TabIndex = 8;
+            this.comboBox3.Text = "通用推挽输出";
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("SimSun", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(39, 139);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(480, 26);
+            this.textBox1.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(409, 211);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "生成寄存器语句";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "端口时钟";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(138, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "引脚";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(252, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "IO工作模式";
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "2MHz",
+            "10MHz",
+            "50MHz"});
+            this.comboBox4.Location = new System.Drawing.Point(420, 72);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(101, 20);
+            this.comboBox4.TabIndex = 14;
+            this.comboBox4.Text = "50MHz";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(418, 36);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 12);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "输出模式最大速度";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("SimSun", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(38, 214);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(279, 20);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "stm32IO口方向寄存器语句生成";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("SimSun", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label6.Location = new System.Drawing.Point(52, 264);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(455, 42);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "例子：\r\n#define PA11_IO_IN()  {GPIOA->CRH&=0XFFFF0FFF;GPIOA->CRH|=8<<12;}\r\n#define P" +
+    "A11_IO_OUT() {GPIOA->CRH&=0XFFFF0FFF;GPIOA->CRH|=3<<12;}";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("SimSun", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.linkLabel1.Location = new System.Drawing.Point(455, 339);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(64, 16);
+            this.linkLabel1.TabIndex = 18;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "MY HOME";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("SimSun", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label7.Location = new System.Drawing.Point(291, 339);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(136, 16);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Design By Mengke";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(564, 381);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "IO方向设置语句生成器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedItem.ToString())
+            {
+                //case "A": MessageBox.Show("A"); break;
+                //case "B": MessageBox.Show("B"); break;
+                //case "C": MessageBox.Show("C"); break;
+                //case "D": MessageBox.Show("D"); break;
+                //case "E": MessageBox.Show("E"); break;
+                //case "F": MessageBox.Show("F"); break;
+                //case "G": MessageBox.Show("G"); break;
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox2.SelectedItem.ToString())
+            {
+                //case "0": MessageBox.Show("0"); break;
+                //case "1": MessageBox.Show("1"); break;
+                //case "2": MessageBox.Show("2"); break;
+                //case "3": MessageBox.Show("3"); break;
+                //case "4": MessageBox.Show("4"); break;
+                //case "5": MessageBox.Show("5"); break;
+                //case "6": MessageBox.Show("6"); break;
+                //case "7": MessageBox.Show("7"); break;
+                //case "8": MessageBox.Show("8"); break;
+                //case "9": MessageBox.Show("9"); break;
+                //case "10": MessageBox.Show("10"); break;
+                //case "11": MessageBox.Show("11"); break;
+                //case "12": MessageBox.Show("12"); break;
+                //case "13": MessageBox.Show("13"); break;
+                //case "14": MessageBox.Show("14"); break;
+            }
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox3.SelectedItem.ToString())
+            {
+                //case "浮空输入": MessageBox.Show("浮空输入"); break;
+                //case "模拟输入": MessageBox.Show("模拟输入"); break;
+                //case "上拉输入": MessageBox.Show("上拉输入"); break;
+                //case "下拉输入": MessageBox.Show("下拉输入"); break;
+                //case "推挽输出": MessageBox.Show("推挽输出"); break;
+                //case "开漏输出": MessageBox.Show("开漏输出"); break;
+                //case "复用开漏输出": MessageBox.Show("复用开漏输出"); break;
+                //case "复用推挽输出": MessageBox.Show("复用推挽输出"); break;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string text = "";
+            int pin = 0;      //管脚
+            string CR = "";   //CRL，CRH
+            string POS = "0xFFFFFFFF";  //位数
+            int x16 = 0;    //      
+
+            int CNFy = 0;   //端口的配置位
+            int MODEy = 0;  //端口的模式位
+            string mode = "";
+
+            pin = int.Parse(comboBox2.Text);
+            
+            if (pin> 7)
+            {
+                CR = "H";
+                POS = Regex.Replace(POS, @"(?<=^(.){" + (17-pin) + "}).", "0");
+                x16 = (pin - 8) * 4;
+            }
+            else
+            {
+                CR = "L";
+                POS = Regex.Replace(POS, @"(?<=^(.){" + (9-pin) + "}).", "0");
+                x16 = pin * 4;
+            }
+
+            switch (comboBox3.Text)
+            {                                            //   CNFy  / MODEy
+                case "浮空输入": mode = "4"; break;       //   01      00
+                case "模拟输入": mode = "0"; break;       //   00      00
+                case "上拉输入": mode = "8"; break;       //   10      00
+                case "下拉输入": mode = "8"; break;       //   10      00
+                case "通用推挽输出":                      //   00      11/01/10
+                    switch (comboBox4.Text)
+                    {
+                        case "2MHz": mode = "2"; break;  //   00      10
+                        case "10MHz": mode = "1"; break; //   00      01
+                        case "50MHz": mode = "3"; break; //   00      11
+                    }
+                    break;  
+                case "通用开漏输出":                     //    01      11/01/10
+                    switch (comboBox4.Text)
+                    {
+                        case "2MHz": mode = "6"; break;  //    01      10
+                        case "10MHz": mode = "5"; break; //    01      01
+                        case "50MHz": mode = "7"; break; //    01      11
+                    }
+                    break; 
+                case "复用开漏输出":                      //    10      11/01/10
+                    switch (comboBox4.Text)
+                    {
+                        case "2MHz": mode = "10"; break; //    10      10
+                        case "10MHz":  mode = "9"; break;//    10      01
+                        case "50MHz": mode = "11"; break;//    10      11
+                    }
+                    break;  
+                case "复用推挽输出":                      //    11      11/01/10    
+                    switch (comboBox4.Text)
+                    {
+                        case "2MHz": mode = "14"; break; //    11      10
+                        case "10MHz": mode = "13"; break;//    11      01
+                        case "50MHz": mode = "15"; break;//    11      11
+                    }
+                    break;   
+            }
+
+            //00 11
+            //10 00
+            //#define DHT11_IO_IN()  {GPIOA->CRH&=0XFFFF0FFF;GPIOA->CRH|=8<<12;}
+            //#define DHT11_IO_OUT() {GPIOA->CRH&=0XFFFF0FFF;GPIOA->CRH|=3<<12;}										   
+
+            text = "{GPIO" + comboBox1.Text + "->CR" + CR+"&="+POS+";GPIO"+comboBox1.Text+"->CR"+CR+"|="+mode+"<<"+ x16+";}";
+            textBox1.Text = text;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://47.106.209.211/");
+        }
+    }
+}
